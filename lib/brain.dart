bc5635a69087ae2bbe6020e2e257cf86db9839b4
Keanu <keanujames5567@gmail.com>
2025-10-22 15:32:18 +0800

    commit changes

--- lib/brain.dart
+++ lib/brain.dart
@@ -1,6 +1,13 @@
 class Brain {
   String owner;
+
   Brain({this.owner = 'Unknown'});
-  void think(String task) => print('[Brain] Thinking: $task');
-  void dream() => print('[Brain] Dreaming...');
+
+  void think(String task) {
+    print('[Brain] Thinking: $task');
+  }
+
+  void dream() {
+    print('[Brain] Dreaming...');
+  }
 }
