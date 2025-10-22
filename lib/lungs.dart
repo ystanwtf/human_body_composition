6480526ce49408211d0ed13888de27ec7fb37970
Kirby <kirby7829@gmail.com>
2025-10-22 16:27:44 +0800

    Add human body feature code

--- lib/lungs.dart
+++ lib/lungs.dart
@@ -1,7 +1,17 @@
+// lib/lungs.dart
+// Member 4: Creates the Lungs part class
+// Has one property and two methods that the HumanBody container will call
+
 class Lungs {
-  double capacity;
+  double capacity; // liters of air capacity
+
   Lungs({this.capacity = 5.0});
 
-  void breathe() => print('[Lungs] Breathing with $capacity L capacity');
-  void slowBreath() => print('[Lungs] Breathing slowly...');
+  void breathe() {
+    print('[Lungs] Breathing with $capacityL capacity');
+  }
+
+  void slowBreath() {
+    print('[Lungs] Breathing slowly...');
+  }
 }
